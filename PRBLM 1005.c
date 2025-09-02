b6540c2654051a9b58230f7b6b8f8ed7d8a334b0
sheiknusrathasan <sheiknusrathasan@gmail.com>
2025-09-03 00:28:51 +0600

    Add files via upload

---
+++ PRBLM 1005.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+double A,B,X,MEDIA;
+scanf("%lf %lf", &A, &B);
+A=A*3.5;
+B=B*7.5;
+X=A+B;
+MEDIA=X/11;
+printf("MEDIA=%.5lf",MEDIA);
+return 0;
+}
---
+++ prblm1002.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#define pi 3.1416
+
+int main(){
+
+double R, A;
+
+scanf("%lf", &R);
+
+A = pi*R*R;
+
+printf("A = %.4lf", A);
+
+return 0;
+}
